425d55cc3b6de931ed1e122483a76673e25fcbb4
joseadolfo10 <139913282+joseadolfo10@users.noreply.github.com>
2023-10-17 10:16:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function js_style() {
+    
+    var paragraph = document.getElementById('text');
+
+    paragraph.style.fontFamily = 'Arial, sans-serif';
+    paragraph.style.fontSize = '24px';
+    paragraph.style.color = 'blue';
+}
